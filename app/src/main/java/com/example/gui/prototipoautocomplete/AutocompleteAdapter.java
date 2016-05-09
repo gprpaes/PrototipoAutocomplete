19145fd8533e77c06817b578ede7d27c4c8da1d4
Guilherme <guilhermep98@gmail.com>
2016-05-08 21:31:12 -0300

    Autocomplete funcionando

---
+++ app/src/main/java/com/example/gui/prototipoautocomplete/AutocompleteAdapter.java
@@ -0,0 +1,155 @@
+package com.example.gui.prototipoautocomplete;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filterable;
+import android.widget.TextView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.data.DataBufferUtils;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLngBounds;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class AutocompleteAdapter extends RecyclerView.Adapter<AutocompleteAdapter.PlaceViewHolder> implements Filterable
+{
+
+    private ArrayList<AutocompletePrediction> lstResultados;
+    private GoogleApiClient mGoogleApiClient;
+    private LatLngBounds bounds;
+    private AutocompleteFilter filtro;
+    private Context context;
+
+
+    public AutocompleteAdapter(Context context, GoogleApiClient mGoogleApiClient, LatLngBounds bounds, AutocompleteFilter filtro){
+        this.context = context;
+        this.mGoogleApiClient = mGoogleApiClient;
+        this.bounds = bounds;
+        this.filtro = filtro;
+
+    }
+
+    public void setBounds(LatLngBounds bounds){
+        this.bounds = bounds;
+    }
+
+    @Override
+    public Filter getFilter(){
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint){
+                FilterResults results = new FilterResults();
+
+                if(constraint != null){
+                    lstResultados = getAutocomplete(constraint);
+                    if(lstResultados != null){
+                        results.values = lstResultados;
+                        results.count = lstResultados.size();
+                    }
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results){
+                if(results != null && results.count > 0)
+                    notifyDataSetChanged();
+            }
+
+            @Override
+            public CharSequence convertResultToString(Object resultado){
+                if(resultado instanceof AutocompletePrediction){
+                    return ((AutocompletePrediction) resultado).getFullText(null);
+                }else{
+                    return super.convertResultToString(resultado);
+                }
+
+            }
+
+
+        };
+
+    }
+
+    private ArrayList<AutocompletePrediction> getAutocomplete(CharSequence constraint){
+        if(mGoogleApiClient.isConnected()){
+            Log.i("getAutoComplete", "Query iniciando para:" +constraint);
+            PendingResult<AutocompletePredictionBuffer> resultados =
+                    Places.GeoDataApi
+                            .getAutocompletePredictions(mGoogleApiClient, constraint.toString(), bounds,filtro);
+            AutocompletePredictionBuffer autocompletePredictions = resultados.await(60, TimeUnit.SECONDS);
+
+            final Status status = autocompletePredictions.getStatus();
+
+            if(!status.isSuccess()){
+                Toast.makeText(context, "Erro ao conectar com a API" + status.toString()
+                        ,Toast.LENGTH_SHORT).show();
+                autocompletePredictions.release();
+                return null;
+
+            }
+
+            Log.i("getAutoComplete", "Query finalizada, resultados:"+autocompletePredictions.getCount());
+            return DataBufferUtils.freezeAndClose(autocompletePredictions);
+
+
+
+
+
+        }
+
+        Log.e("Api Client", "Não foi possível conectar a GoogleApiCliente");
+        return null;
+
+    }
+
+
+
+    public class PlaceViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+
+        public PlaceViewHolder(View view){
+            super(view);
+            textView = (TextView) view.findViewById(R.id.txtPlace);
+        }
+    }
+
+    @Override
+    public PlaceViewHolder onCreateViewHolder(ViewGroup viewgroup,int i){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View convertView = inflater.inflate(R.layout.item,viewgroup,false);
+        PlaceViewHolder viewHolder = new PlaceViewHolder(convertView);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(PlaceViewHolder viewHolder, final int i){
+
+        viewHolder.textView.setText(lstResultados.get(i).getPrimaryText(null));
+    }
+
+    @Override
+    public int getItemCount(){
+        if(lstResultados != null){
+            return lstResultados.size();
+        }else {
+            return 0;
+        }
+    }
+}
--- app/src/main/java/com/example/gui/prototipoautocomplete/MainActivity.java
+++ app/src/main/java/com/example/gui/prototipoautocomplete/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.gui.prototipoautocomplete;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+
+    public GoogleApiClient googleApiClient;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-0,0), new LatLng(0,0));
+    private EditText autoComplete;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private AutocompleteAdapter autocompleteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,0,this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+
+        autoComplete = (EditText)findViewById(R.id.edtAutocomplete);
+        autocompleteAdapter = new AutocompleteAdapter(this,googleApiClient,LAT_LNG_BOUNDS,null);
+        recyclerView = (RecyclerView) findViewById(R.id.recLista);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(autocompleteAdapter);
+
+        autoComplete.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().equals("") && googleApiClient.isConnected()){
+                    autocompleteAdapter.getFilter().filter(s);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
